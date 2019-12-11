cb11de376f8d98cbdb1f79f50233362083ad3707
cp <cp@lt-cp-2017>
2019-12-11 16:07:06 +0100

    deadlock desactivated but comment to reproduce it

--- ialtem/threads/moreExamples/src/dining/DiningPhilosophers.java
+++ ialtem/threads/moreExamples/src/dining/DiningPhilosophers.java
@@ -16,11 +16,12 @@ public class DiningPhilosophers {
             Object leftFork = forks[i];
             Object rightFork = forks[(i + 1) % forks.length];
 
- //           if (i == philosophers.length - 1) {
- //               philosophers[i] = new Philosopher(rightFork, leftFork); // The last philosopher picks up the right fork first
- //           } else {
+            // comment the following to see a deadlock occurring
+            if (i == philosophers.length - 1) {
+                philosophers[i] = new Philosopher(rightFork, leftFork); // The last philosopher picks up the right fork first
+            } else {
                 philosophers[i] = new Philosopher(leftFork, rightFork);
- //           }
+            }
 
             Thread t = new Thread(philosophers[i], "Philosopher " + (i + 1));
             t.start();
